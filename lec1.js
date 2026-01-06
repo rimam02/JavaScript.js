435a6b276901768ef0b7176ffdf6608b6e13445a
rimam02 <2025.rimam@isu.ac.in>
2026-01-06 11:15:49 +0530

    Add files via upload

---
+++ lec1.js
@@ -0,0 +1,27 @@
+// // saying hello (silngle line commit)
+// /* (multiple commit)
+// */ 
+// console.log("Hello, World!");
+
+// let fname = "Rima";
+// fname[0] = "1";
+// console.log(fname);
+
+// console.log(fname.toUpperCase());
+
+
+// // concatenation
+// let sayHello = "Hello";
+// console.log(sayHello, fname); //Hello Rima
+
+// let greet = sayHello + " " + fname;
+// console.log(greet); //Hello Rima
+
+
+// // strings literals
+// console.log(`${sayHello} ${fname}`)
+// // let age = 28
+// // console.log(typeof fname);
+"use strict";
+placement = null;
+console.log(placement);
